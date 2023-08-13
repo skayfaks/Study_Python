900f3ebd8128d2cbece386a9df215a8a7b759f0f
Anton K <skayfaks@gmail.com>
2023-08-13 19:03:53 +0300

    вложенные списки

---
+++ 04 Функции Python/030 intro.py
@@ -0,0 +1,24 @@
+# Встроенные ФУНКЦИИ
+
+x=print('Hello')
+y=set()
+print(type(x))
+print(type(y))
+#<class 'NoneType'>
+#<class 'set'>
+
+# Встроенные МЕТОДЫ  - это функциикоторые есть у объектов
+
+my_lisst=[1,2,3]
+my_lisst.append(777777)
+print(my_lisst)
+#[1, 2, 3, 777777]
+
+my_lisst.clear()
+print(my_lisst)
+#[]
+#       https://docs.python.org/3/index.html
+#       https://docs.python.org/3/library/functions.html
+
+
+# Так же можно создавать свои функции
